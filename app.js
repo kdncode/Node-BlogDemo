7a26457949433c31fce9cc89e5bf12975c2caeb7
kdncode <kdncode@gmail.com>
2018-04-26 23:20:45 -0500

    add public & views folders

---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
+var app = express()
+
+// App Config
+mongoose.connect("mongodb://localhost/restfull_blog_app")
+
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+
+// Mongoose/Model Config
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: { type: Date, default: Date.now }
+})
+
+// Compile to model
+var Blog = mongoose.model("Blog", blogSchema)
+
+
+// RESTFUL Routes
+app.get("/", (req, res) => {
+    res.redirect("/blogs")
+})
+
+
+app.get("/blogs", (req, res) => {
+    // Retrive the blogs from the DB
+    Blog.find({}, (err, blogs) => {
+        if(err) { console.log("error")}
+        else    { res.render("index", {blogs: blogs}) }
+    })
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("blog is running")
+})
